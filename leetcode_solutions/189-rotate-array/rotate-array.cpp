df15b5bc3ea2b3f1d43d76e315507459f22de0ce
MayankChhatani <mayankchhatani1@gmail.com>
2026-08-03 03:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 29.6 MB (70.85%) - LeetSync

--- leetcode_solutions/189-rotate-array/rotate-array.cpp
+++ leetcode_solutions/189-rotate-array/rotate-array.cpp
@@ -1,21 +1,11 @@
-                        //  brute force
+            // optimal approach  -array manipulation
 
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int>arr;
-        int n=nums.size();
-        // int l=0;
-
-        k = k%n ; // k kabhi kabhi n se bada bhi to ho sat ahi
-        for(int i=n-k;i<n;i++){             //Mistake 2: k actually split point nahi hai 
-            arr.push_back(nums[i]);             //  so split point kya hoga n - k
-            // l = i;
-        }
-        // reverse(arr.begin(),arr.end());
-        for(int i=0;i<n-k;i++){
-            arr.push_back(nums[i]);
-        }
-        nums = arr;
+        k = k%nums.size();
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
