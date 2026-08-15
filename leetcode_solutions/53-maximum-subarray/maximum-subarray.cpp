552280a73edcc2046d07b1dcc8b6a3d8224e0a91
MayankChhatani <mayankchhatani1@gmail.com>
2026-08-15 15:37:57 +0530

    Time: 7 ms (4.44%) | Memory: 71.9 MB (15.17%) - LeetSync

--- leetcode_solutions/53-maximum-subarray/maximum-subarray.cpp
+++ leetcode_solutions/53-maximum-subarray/maximum-subarray.cpp
@@ -2,25 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        // int bestEnd = nums[0];   mai runningSum bolunga
         int runningSum = nums[0];
-        // runningSum stores the maximum subarray sum ending at the current index
-        int ans = nums[0];
+        int maximumSum = nums[0];
 
         for(int i=1;i<n;i++){
-            int sum1 = runningSum + nums[i];
-            int sum2 = nums[i];
-
-            runningSum = max(sum1, sum2);
-            ans = max(runningSum, ans); 
+            int sum1 = nums[i];
+            runningSum = max(runningSum + nums[i], sum1);
+            maximumSum = max(maximumSum, runningSum);
         }
-        return ans;
+        return maximumSum;
     }
 };
-
-
-// "It doesn't look like Kadane. Why?"
-
-// To tu defensive mat hona. Explain kar:
-
-// "The implementation may not look like the traditional Kadane's Algorithm because I am not using an explicit variable like currentSum or maxSum, but the logic is exactly Kadane's. At every index, I am calculating the maximum subarray sum ending at that index by taking the maximum of extending the previous subarray or starting a new one."
