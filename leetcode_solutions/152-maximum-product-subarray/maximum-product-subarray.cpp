9cf2a7b2d301d2df2dede479cae262750824f20b
MayankChhatani <mayankchhatani1@gmail.com>
2026-08-15 17:59:57 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (23.90%) - LeetSync

--- leetcode_solutions/152-maximum-product-subarray/maximum-product-subarray.cpp
+++ leetcode_solutions/152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        int n= nums.size();
-        int maxEnd = nums[0];
-        int minEnd = nums[0];
-        int ans = nums[0];
+        int n = nums.size();
+        int minProd = nums[0];
+        int maxProd = nums[0];
+        int maxiOfAll = nums[0];
+        // int runningProd = nums[0];
 
-        for(int i = 1; i < n;i++){
-            int pro1 = nums[i];
-            int pro2 = maxEnd * nums[i];   // maximum product nikalne ka path
-            int pro3 = minEnd * nums[i];   // minend product 
+        for(int i = 1;i<n;i++){
+            int prod1 = nums[i];
+            
+            // maxProd = max(maxProd, max(minProd * prod,prod));
+            // minProd = min(minProd * prod, prod);
 
-            maxEnd = max(pro1,max(pro2, pro3));
-            minEnd = min(pro1,min(pro2, pro3));
+            //core idea miss kar gye chhatani ji
 
-            ans = max(ans,maxEnd);
+            // purane wale ko minend aur maxend ko bhi to store krna padega na 
+
+            int prod2 = minProd * prod1;
+            int prod3 = maxProd * prod1;
+
+            // maxProd = max(maxProd, max(prod2,prod3));   // yaha bhi mai mai main objective hi bhulgya har baar current state ko use karna chaiye 
+            // minProd = min(minProd, min(prod2,prod3));
+            maxProd = max(prod1, max(prod2,prod3));
+            minProd = min(prod1, min(prod2,prod3));
+
+
+            maxiOfAll = max(maxiOfAll,maxProd);    
         }
-        return ans;
+        return maxiOfAll;
     }
 };
+
