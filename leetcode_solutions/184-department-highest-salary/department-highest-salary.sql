59ba3c76f4a2e94e3187ba4669e088d7c6a0920f
MayankChhatani <mayankchhatani1@gmail.com>
2026-08-21 01:17:18 +0530

    Time: 1114 ms (20.45%) | Memory: 0B (100.00%) - LeetSync

--- leetcode_solutions/184-department-highest-salary/department-highest-salary.sql
+++ leetcode_solutions/184-department-highest-salary/department-highest-salary.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
-select d.name as Department,
-        e.name as Employee,
-        e.salary as Salary
-from Employee e
-left join Department d
-on e.departmentId = d.id
-where e.salary = (
-    select Max(salary)
-    from Employee
+SELECT   d.name AS Department,
+         e.name AS Employee,
+        e.salary AS Salary
+FROM Employee e
+LEFT JOIN Department d
+ON e.departmentID = d.id
+WHERE e.salary = (
+    SELECT MAX(salary)
+    from Employee 
     where departmentId = e.departmentId
-    -- Bhai, current employee jis department mein hai, sirf usi department ke andar maximum salary nikaal.
-)
+);
+-- GROUP BY e.salary (Department)
